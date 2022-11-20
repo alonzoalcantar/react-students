37ebc70a270cb338d0c886a839aeb9ddfbc68165
alonzoalcantar <pinoa.03@outlook.com>
2022-11-19 22:15:31 -0700

    commmiting to submit

--- src/Score.jsx
+++ src/Score.jsx
@@ -2,13 +2,17 @@ export default function Score ({student}) {
     return(
         <div>
         {student.scores.map((student, idx) => (
+
+
             <div>
-                <label>Date: </label>
+                <strong><label>Date: </label></strong>
                 <p>{student.date}</p>
-                <label>Score:</label>
+                <strong><label>Score:</label></strong>
                 <p>{student.score}</p>
                 <hr></hr>
             </div>
+
+
         ))}
         </div>
     )
--- src/Student.jsx
+++ src/Student.jsx
@@ -4,18 +4,22 @@ export default function Student ({ students }) {
     return (
         <ul>
             {students.map((student, idx) => (
+
+
                 <li>
-                    <h1><strong>{student.name}</strong></h1>
+                    <h1>{student.name}</h1>
                     <p>{student.bio}</p>
 
                     <div>
-                        <label><strong>Score Information</strong></label>
+                        <h3>Score Information</h3>
                         <Score 
                         student= {student}
                         idx={idx} />
                     </div>
 
                 </li>
+
+
             ))}
         </ul>
     )
